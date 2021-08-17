e7c75335c61642f06b5408cadfed24709b926459
haeseung-lee <dlgotmd93@gmail.com>
2021-08-17 23:36:29 +0900

    0817..k번째 숫자구하기 풀이중

---
+++ src/codingTest/practice/hash/KthNumber.java
@@ -0,0 +1,15 @@
+package codingTest.practice.hash;
+
+//k번째 숫자 구하기
+public class KthNumber {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public static int[] solution(int[] array, int[][] commands) {
+		int[] answer = {};
+		return answer;
+	}
+}
--- src/codingTest/practice/hash/NonCompletionUser.java
+++ src/codingTest/practice/hash/NonCompletionUser.java
@@ -2,9 +2,11 @@ package codingTest.practice.hash;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 //완주하지 못한 선수2
 //https://programmers.co.kr/learn/courses/30/lessons/42576
+//what is getOrDefault?
 public class NonCompletionUser {
 	
 	public static void main(String[] args) {
@@ -35,5 +37,19 @@ public class NonCompletionUser {
 		System.out.println(answer);
 	}
 	
+	//다른사람풀이 - 분석해보기
+	public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
+        }
+        return answer;
+    }
 	
 }
