dcb976a650b7984ffae763520405fa42af144b39
haeseung-lee <dlgotmd93@gmail.com>
2021-08-20 23:03:43 +0900

    0820..모의고사 최다 정답자 구하기

---
+++ src/codingTest/practice/fullSearch/MockTest.java
@@ -0,0 +1,24 @@
+package codingTest.practice.fullSearch;
+
+public class MockTest {
+	
+	//3명이 있다.
+	//1번이 찍는 방식 -> 1,2,3,4,5,1,2,3,4,5...(5개 주기로 반복)
+	//2번이 찍는 방식 -> 2,1,2,3,2,4,2,5,2,1,2,3,...(8개주기 반복)
+	//3번이 찍는 방식 -> 3,3,1,1,2,2,4,4,5,5,3,3,1,1,2,2,,4,5,5,...(10개 주기로 반복)
+	
+	public static void main(String[] args) {
+		
+	}
+	
+    public int[] solution(int[] answers) {
+
+    	
+    	int[] answer = {};
+        
+        
+        return answer;
+    }
+	
+
+}
