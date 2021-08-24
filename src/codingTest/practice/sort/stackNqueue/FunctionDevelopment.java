572dc35ca1d8d21dec241c0f24868499dd0c8573
haeseung-lee <dlgotmd93@gmail.com>
2021-08-24 23:44:20 +0900

    0824..개발문제 풀이중..commit

--- src/codingTest/practice/sort/stackNqueue/FunctionDevelopment.java
+++ src/codingTest/practice/sort/stackNqueue/FunctionDevelopment.java
@@ -1,5 +1,9 @@
 package codingTest.practice.sort.stackNqueue;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FunctionDevelopment {
 
 	public static void main(String[] args) {
@@ -10,6 +14,9 @@ public class FunctionDevelopment {
     	//progresses : 먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수배열
     	//speeds : 각 작업의 개발속도가 적힌 정수배열
     	//배포는 1일 1회, 하루의 끝에
+    	
+    	//1. 선입선출을 위한 queue 사용
+    	Queue<Object> progress = new LinkedList<>(Arrays.asList(progresses));
         int[] answer = {};
         return answer;
     }
