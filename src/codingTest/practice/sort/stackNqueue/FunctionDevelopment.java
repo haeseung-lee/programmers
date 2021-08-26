ca0e1b52df05a52b410e3dfab608ed4ad209e608
haeseung-lee <dlgotmd93@gmail.com>
2021-08-26 23:26:56 +0900

    0826..개발문제 풀이중..

--- src/codingTest/practice/sort/stackNqueue/FunctionDevelopment.java
+++ src/codingTest/practice/sort/stackNqueue/FunctionDevelopment.java
@@ -1,6 +1,7 @@
 package codingTest.practice.sort.stackNqueue;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -8,6 +9,11 @@ public class FunctionDevelopment {
 
 	public static void main(String[] args) {
 		
+		FunctionDevelopment test = new FunctionDevelopment();
+		
+		int[] progresses = {93,30,55}; 
+		int[] speeds = {1,30,5}; 
+		test.solution(progresses, speeds);
 	}
 	
     public int[] solution(int[] progresses, int[] speeds) {
@@ -16,7 +22,34 @@ public class FunctionDevelopment {
     	//배포는 1일 1회, 하루의 끝에
     	
     	//1. 선입선출을 위한 queue 사용
-    	Queue<Object> progress = new LinkedList<>(Arrays.asList(progresses));
+//    	for (int i : progresses) {
+//			System.out.println(i);
+//		}
+    	Queue progress = new LinkedList<>(Arrays.asList(progresses));
+    	
+    	Queue test = new LinkedList<>();
+    	
+    	for (int object : progresses) {
+    		test.add(object);
+    		
+		}
+    	System.out.println(test);
+    	for(int i = 0 ; i < progresses.length; i++) {
+    		test.poll();
+    	}
+    	
+//    	while(progresses.length ==0) {
+//
+//    		for(int i = 0; i < progresses.length; i++) {
+//    			
+//    			progresses[i] = progresses[i] + speeds[i];
+//    			
+//    			if(progresses[0] >= 100) {
+//    				
+//    			}
+//    		}
+//    	}
+    	
         int[] answer = {};
         return answer;
     }
