65c22894f49d39dd6c026150c5ef50f82b014086
haeseung-lee <dlgotmd93@gmail.com>
2021-09-23 21:40:51 +0900

    0923..상호평가..

---
+++ src/codingTest/practice/weeklyChallenge/MutualEvaluation.java
@@ -0,0 +1,24 @@
+package codingTest.practice.weeklyChallenge;
+
+/**
+ * 상호평가
+ * @author 이해승
+ *
+ */
+public class MutualEvaluation {
+
+	public static void main(String[] args) {
+		
+	}
+	
+    /**
+     * @param scores  점수 i번 학생이 평가한 j번 학생의 과제 점수
+     * @return
+     */
+    public String solution(int[][] scores) {
+    	
+    	
+        String answer = "";
+        return answer;
+    }
+}
