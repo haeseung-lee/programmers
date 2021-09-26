9cf5285fc1fc00f123bb426a5565d7427abe9970
haeseung-lee <dlgotmd93@gmail.com>
2021-09-26 23:46:38 +0900

    0926..모음사전

---
+++ src/codingTest/practice/weeklyChallenge/VowelDictionary.java
@@ -0,0 +1,23 @@
+package codingTest.practice.weeklyChallenge;
+
+/**
+ * 모음사전..
+ * @author user
+ * 
+ */
+public class VowelDictionary {
+
+	public static void main(String[] args) {
+		
+	}
+	
+    public int solution(String word) {
+    	
+    	/*A E I O U 로 사전..
+    	 * 순서는 A, AA... 
+    	 * 길이는 5이하
+    	 * */
+        int answer = 0;
+        return answer;
+    }
+}
