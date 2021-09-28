771840ce6b30bcea673b88a1a37ce3998b5603c8
haeseung-lee <dlgotmd93@gmail.com>
2021-09-28 23:45:27 +0900

    0928..모음사전

--- src/codingTest/practice/weeklyChallenge/VowelDictionary.java
+++ src/codingTest/practice/weeklyChallenge/VowelDictionary.java
@@ -1,5 +1,9 @@
 package codingTest.practice.weeklyChallenge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 모음사전..
  * @author user
@@ -9,6 +13,9 @@ public class VowelDictionary {
 
 	public static void main(String[] args) {
 		
+		VowelDictionary test = new VowelDictionary();
+		
+		System.out.println(test.solution("test"));
 	}
 	
     public int solution(String word) {
@@ -22,6 +29,16 @@ public class VowelDictionary {
     	
     	String[] vowelList = {"A","E","I","O","U"};
     	
+    	List<String> vowel = Arrays.asList(vowelList);
+    	
+    	//길이가 1인 경우 : 5
+    	//길이가 2인 경우 : 15개 AA, AE, AI, AO, AU, EE, EI, EO, EU, II, IO, IU, OO, OU, UU  / 5 + 4 + 3 + 2 + 1
+    	//길이가 3인 경우 : AAA, AAE, AAI, AAO, AAU, AEA, AEE, AEI, AEO, AEU, AIA, AIE, AII, AIO, AIU, AOA, AOE, AOI,AOO, AOU, AUA, AUE, AUI, AUO, AUU, 25+24+23+22+21
+    	
+    	for(int i = 0; i < vowelList.length; i++) {
+    		
+    	}
+    	System.out.println(vowel);
     	
         int answer = 0;
         return answer;
